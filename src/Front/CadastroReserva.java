937071b25d1cf3428045e7e400758b6ffe2721f0
Daniel Rodrigues Freitas <06daniel.freitas@gmail.com>
2025-07-08 17:58:08 -0300

    ultima do dalade

---
+++ src/Front/CadastroReserva.java
@@ -0,0 +1,14 @@
+package Front;
+
+import java.time.LocalDateTime;
+
+import entities.EspacoFisico;
+
+public class CadastroReserva {
+
+    public boolean reservar(EspacoFisico espaco, LocalDateTime inicio, LocalDateTime fim) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'reservar'");
+    }
+
+}
