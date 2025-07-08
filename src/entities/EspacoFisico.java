0eb52f6acf465e1fe3d6a0a9be844178603cf2ba
Derick <wendellderick@gmail.com>
2025-07-08 11:21:52 -0300

    Derick

--- src/entities/EspacoFisico.java
+++ src/entities/EspacoFisico.java
@@ -4,21 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import Cadastros.CadastroEspacoFisico.Equipamentos;
 
-<<<<<<< HEAD
-public abstract class EspacoFisico { 
-    protected String sala;
-    protected String tipo;
-    protected String capacidade;
-    protected String localizacao;
-    protected List<Equipamentos> equipamentos;  
-=======
-public class EspacoFisico {  // Classe abstrata (não pode ser instanciada diretamente)
-	public String nome;
+
+public class EspacoFisico { 
+	public String sala;
     public String tipo;
     public String capacidade;
     public String localizacao;
     public List<Equipamentos> equipamentos;  // Agora é uma lista de objetos!
->>>>>>> 2d16a118a3d9f22161e882012a282f79ee715b17
 
     // Construtor
     public EspacoFisico(String sala, String tipo, String capacidade, String localizacao, String equipamentos) {
